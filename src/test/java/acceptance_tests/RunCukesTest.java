fdff680568645114e9d24011030300f7ae17fa71
Richard Chernanko <rchernanko@hotmail.co.uk>
2016-01-08 17:34:22 +0000

    adding cucumber options to my RunCukesTests class + am also adding 2 failing scenarios to the feature file

--- src/test/java/acceptance_tests/RunCukesTest.java
+++ src/test/java/acceptance_tests/RunCukesTest.java
@@ -5,6 +5,6 @@ import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions (features = "src/test/resources/features/")
+@CucumberOptions (glue = {"step_definitions"}, features = "src/test/resources/features/")
 public class RunCukesTest {
 }
