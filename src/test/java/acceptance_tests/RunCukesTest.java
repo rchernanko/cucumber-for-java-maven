b53b858036ff60af57259716d3ea34b01f136d36
rchernanko <rchernanko@hotmail.co.uk>
2016-01-13 22:29:15 +0000

    adding some comments on how to specify feature / scenario tags via the cucumber options and via the command line

--- src/test/java/acceptance_tests/RunCukesTest.java
+++ src/test/java/acceptance_tests/RunCukesTest.java
@@ -8,3 +8,29 @@ import org.junit.runner.RunWith;
 @CucumberOptions (glue = {"step_definitions"}, features = "src/test/resources/features/")
 public class RunCukesTest {
 }
+
+/*
+
+RUNNING SCENARIOS / FEATURES AS PER THEIR TAGS
+
+1)
+
+If I wanted to run all scenarios that have a @richard tag EVERY time when I enter mvn clean test, I could add the below
+tags section to my Cucumber Options:
+
+@CucumberOptions (glue = {"step_definitions"}, features = "src/test/resources/features/", tags = {"@richard"})
+
+2)
+
+Of course, if I don't have a tags section in my cucumber options, then I can also specify specific tags to run on the
+command line:
+
+mvn clean test -Dcucumber.options="--tags @richard --tags @hello"
+
+3)
+
+If I want to run all scenarios in the features directory specified in the cucumber options, I simply don't add a
+tags section to the cucumber options above. Then, if I run mvn clean test, every feature within the feature directory
+will be run
+
+ */
