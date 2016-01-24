acbd5f1d631e062234e5e9b60b684466d693a736
rchernanko <rchernanko@hotmail.co.uk>
2016-01-24 18:35:52 +0000

    adding selenium commands to the AtmUserInterface class - this is currently failing though (As expected)

--- src/test/java/support/chapter_7/nice_bank/AtmUserInterface.java
+++ src/test/java/support/chapter_7/nice_bank/AtmUserInterface.java
@@ -2,9 +2,27 @@ package support.chapter_7.nice_bank;
 
 import implementation.chapter_7.nice_bank.Account;
 import implementation.chapter_7.nice_bank.Teller;
+import org.openqa.selenium.By;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.events.EventFiringWebDriver;
 
 public class AtmUserInterface implements Teller {
 
+    private final EventFiringWebDriver webDriver;
+
+    public AtmUserInterface() {
+        this.webDriver = new EventFiringWebDriver(new FirefoxDriver());
+    }
+
     public void withdrawFrom(Account account, int pounds) {
+        try{
+            webDriver.get("http://localhost:9988");
+            webDriver.findElement(By.id("Amount"))
+                    .sendKeys(String.valueOf(pounds));
+            webDriver.findElement(By.id("Withdraw")).click();
+        }
+        finally {
+            webDriver.close();
+        }
     }
 }
